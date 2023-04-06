d7ade9b6b4180642ac8a03604ceb931178961c00
Iftu Bin Misbah <samio.cse@gmail.com>
2023-04-06 18:05:15 +0600

    added navbar and a navbar

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Hits](https://hits.sh/github.com/wsamio/Online-Marketplace.svg?style=for-the-badge&label=Total%20views&logo=django)](https://hits.sh/github.com/wsamio/Online-Marketplace/)
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'core/index.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
